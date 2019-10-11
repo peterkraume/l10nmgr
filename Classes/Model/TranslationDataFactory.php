42a981c0b2cd5db0be95d884c137954f6cd24fc9
Cybercraft <info@cybercraft.de>
2019-10-11 16:01:26 +0200

    [TASK] use htmlspecialchars_decode to get rid of XML parser encodings
    Change-Id: I1c7772d4eec17724af264ae36821e6927f4887cd

--- Classes/Model/TranslationDataFactory.php
+++ Classes/Model/TranslationDataFactory.php
@@ -129,7 +129,7 @@ class TranslationDataFactory
                             }
                         }
                         if (!empty($translation[$attrs['table']][$attrs['elementUid']][$attrs['key']])) {
-                            $translation[$attrs['table']][$attrs['elementUid']][$attrs['key']] = str_replace([' &gt; ', ' &lt; '],[' > ', ' < '],$translation[$attrs['table']][$attrs['elementUid']][$attrs['key']]);
+                            $translation[$attrs['table']][$attrs['elementUid']][$attrs['key']] = htmlspecialchars_decode($translation[$attrs['table']][$attrs['elementUid']][$attrs['key']]);
                         }
                     }
                 }
