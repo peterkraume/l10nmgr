a0eb3f4bf14da7a5a7f6c85cafec4ef8a7d62320
Jo Hasenau <info@cybercraft.de>
2021-09-22 13:15:29 +0200

    [BUGFIX] Collections must use variable->count() instead of count(variable)

--- Classes/Model/L10nAccumulatedInformation.php
+++ Classes/Model/L10nAccumulatedInformation.php
@@ -225,7 +225,7 @@ class L10nAccumulatedInformation
                     'pages',
                     Constants::L10NMGR_LANGUAGE_RESTRICTION_FIELDNAME
                 );
-                if (count($languageIsRestricted) > 0) {
+                if ($languageIsRestricted->count() > 0) {
                     $this->excludeIndex['pages:' . $pageId] = 1;
                 }
             }
@@ -268,7 +268,7 @@ class L10nAccumulatedInformation
                                                 $table,
                                                 Constants::L10NMGR_LANGUAGE_RESTRICTION_FIELDNAME
                                             );
-                                            if (count($languageIsRestricted) > 0) {
+                                            if ($languageIsRestricted->count() > 0) {
                                                 $this->excludeIndex[$table . ':' . (int)$row['uid']] = 1;
                                                 continue;
                                             }
